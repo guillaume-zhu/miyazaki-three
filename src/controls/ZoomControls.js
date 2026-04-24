9abdc432e0f336af0c6b41fd8c3556242a2ef221
ramynbl <r_nebili@hetic.eu>
2026-04-24 11:28:33 +0200

    fix: setup camera et fix zoom control

--- src/controls/ZoomControls.js
+++ src/controls/ZoomControls.js
@@ -1,15 +1,10 @@
-import * as THREE from "three"
-
 export class ZoomControls {
   constructor(camera, cameraPivot, mouseTracker) {
     this.camera = camera
-    this.cameraPivot = cameraPivot
-    this.mouse = mouseTracker.coords
 
     this.minFov = 15
     this.maxFov = camera.fov
     this.zoomSpeed = 0.05
-    this.raycaster = new THREE.Raycaster()
 
     this.init()
   }
@@ -24,26 +19,8 @@ export class ZoomControls {
 
         if (newFov === oldFov) return
 
-        this.raycaster.setFromCamera(this.mouse, this.camera)
-        const distanceToTarget = this.camera.position.length()
-        const planeNormal = this.camera.position.clone().normalize()
-        const plane = new THREE.Plane(planeNormal, -distanceToTarget * 0.6)
-
-        const pointBefore = new THREE.Vector3()
-        this.raycaster.ray.intersectPlane(plane, pointBefore)
-
         this.camera.fov = newFov
         this.camera.updateProjectionMatrix()
-
-        this.raycaster.setFromCamera(this.mouse, this.camera)
-        const pointAfter = new THREE.Vector3()
-        this.raycaster.ray.intersectPlane(plane, pointAfter)
-
-        if (pointBefore && pointAfter) {
-          const delta = pointBefore.clone().sub(pointAfter)
-          this.cameraPivot.position.x += delta.x
-          this.cameraPivot.position.y += delta.y
-        }
       },
       { passive: true },
     )
